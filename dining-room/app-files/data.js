3c5b3ef1403651eafb2f9605c59625cd0cd74977
shreyashhiwase <122458605+shreyashhiwase@users.noreply.github.com>
2024-04-13 16:40:25 +0530

    added index.html

---
+++ dining-room/app-files/data.js
@@ -0,0 +1,38 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-10c267193e007ea04da31cbc278476e4-02",
+      "name": "10c267193e007ea04da31cbc278476e4-02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.04156944079491254,
+        "pitch": 0.09094348883975023,
+        "fov": 1.3909096906402743
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Dining Room",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
